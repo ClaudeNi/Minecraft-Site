3638ea68815481e4201fde16c478acc782e6dd70
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2021-12-02 19:51:24 +0200

    Add files via upload

--- js/inventory.js
+++ js/inventory.js
@@ -1,1 +1,19 @@
-//
+
+
+const inventory = {
+    "pickaxe": false,
+    "axe": false,
+    "shovel": false,
+    "dirt": 0,
+    "cobblestone": 0,
+    "oak log": 0,
+    "oak leaves": 0,
+};
+
+export function add(block) {
+    inventory[block] += 1;
+}
+
+export function remove(block) {
+    inventory[block] -= 1;
+}
